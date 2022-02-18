1f9b608bb8be1cba328cbdb19221715eab97d25f
davidmbochi <njonge.mbochi@students.ku.ac.ke>
2022-02-18 13:50:20 +0300

    initial commit

---
+++ src/main/java/com/quipbank/countryApp/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.quipbank.countryApp.repository;
+
+import com.quipbank.countryApp.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+    Country findCountryByCountryName(String countryName);
+}
